736677ac59768c318a19c472b0f7113cebbb627e
taher <tah3r.a@gmail.com>
2015-10-26 15:20:17 +0330

    Adding loading map element.

--- public/partials/maps/singular-map-content.php
+++ public/partials/maps/singular-map-content.php
@@ -11,4 +11,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="map_container" style="height: <?php echo absint( $map_height ) ? absint( $map_height ) : '500' ?>px;">
 	<div class="elm google-maps" id="<?php echo esc_attr( $map_id ) ?>" style="height: <?php echo absint( $map_height ) ? absint( $map_height ) : '500' ?>px; padding: 0px; margin: 0px;"></div>
+	<div id="gmap-loading">
+		<?php _e( 'Loading Map', 'elm' ); ?>
+  		<div class="spinner map_loader" id="listing_loader_maps">
+     		<div class="rect1"></div>
+     		<div class="rect2"></div>
+     		<div class="rect3"></div>
+     		<div class="rect4"></div>
+     		<div class="rect5"></div>
+ 		</div>
+	</div>
 </div>
