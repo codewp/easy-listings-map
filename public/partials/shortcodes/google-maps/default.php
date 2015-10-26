f7141e451f111dae9873be1e6b5f46479ffd69f2
taher <tah3r.a@gmail.com>
2015-10-26 15:19:18 +0330

    Adding loading map element.

--- public/partials/shortcodes/google-maps/default.php
+++ public/partials/shortcodes/google-maps/default.php
@@ -18,4 +18,14 @@ if ( strlen( $content ) ) {
 <div class="shortcode map_container" style="height: <?php echo absint( $height ) ? absint( $height ) : '500' ?>px;">
 	<div class="elm google-maps" id="<?php echo esc_attr( $id ) ?>"
 	style="height: <?php echo absint( $height ) ? absint( $height ) : '500' ?>px; padding: 0px; margin: 0px;"></div>
+	<div id="gmap-loading">
+		<?php _e( 'Loading Map', 'elm' ); ?>
+  		<div class="spinner map_loader" id="listing_loader_maps">
+     		<div class="rect1"></div>
+     		<div class="rect2"></div>
+     		<div class="rect3"></div>
+     		<div class="rect4"></div>
+     		<div class="rect5"></div>
+ 		</div>
+	</div>
 </div>
