00498df1a51d516f265503f47ec6ded671e79c2a
taher <tah3r.a@gmail.com>
2016-01-19 05:22:59 +0330

    Using $view_args in view arguments.

--- public/partials/maps/singular-map-content.php
+++ public/partials/maps/singular-map-content.php
@@ -5,16 +5,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * @var ELM_Public_Single_Map $controller
- * @var int                   $map_height
- * @var string                $map_id
+ * @var  array $view_args[
+ *       @type ELM_Public_Single_Map controller
+ *       @type int                   map_height
+ *       @type string                map_id
+ * ]
  */
 
 // Registering scripts and styles.
-$controller->register_scripts();
+$view_args['controller']->register_scripts();
 ?>
-<div class="map_container" style="height: <?php echo absint( $map_height ) ? absint( $map_height ) : '500' ?>px;">
-	<div class="elm google-maps" id="<?php echo esc_attr( $map_id ) ?>" style="height: <?php echo absint( $map_height ) ? absint( $map_height ) : '500' ?>px; padding: 0px; margin: 0px;"></div>
+<div class="map_container" style="height: <?php echo absint( $view_args['map_height'] ) ? absint( $view_args['map_height'] ) : '500' ?>px;">
+	<div class="elm google-maps" id="<?php echo esc_attr( $view_args['map_id'] ) ?>" style="height: <?php echo absint( $view_args['map_height'] ) ? absint( $view_args['map_height'] ) : '500' ?>px; padding: 0px; margin: 0px;"></div>
 	<div id="gmap-loading">
 		<?php _e( 'Loading Map', 'elm' ); ?>
   		<div class="spinner map_loader" id="listing_loader_maps">
