d2602922ab8783c0bc88f9849fb8559e90cddc8d
taher <tah3r.a@gmail.com>
2016-03-16 15:30:08 +0330

    Adding subscribe form to settings menu.

--- admin/partials/menu/settings-menu.php
+++ admin/partials/menu/settings-menu.php
@@ -11,6 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * ]
  */
 
+$subscribed = get_option( '_elm_subscribe', false );
 $active_tab = isset( $_GET['tab'] ) && array_key_exists( $_GET['tab'], $view_args['tabs'] ) ? $_GET['tab'] : 'general';
 ?>
 <div class="wrap">
@@ -29,6 +30,33 @@ $active_tab = isset( $_GET['tab'] ) && array_key_exists( $_GET['tab'], $view_arg
 		}
 		?>
 	</h2>
+	<?php
+	if ( ! $subscribed ) {
+		?>
+		<div class="subscribe-container">
+			<p id="subscribe-info"><strong><?php _e( sprintf( 'Subscribe to our list to get %s of Easy Listings Map and news about it.', '<span>updated documentation</span>' ), 'elm' ); ?></strong></p>
+			<div class="subscribe-form">
+				<div id="subscribe-message" class="message" style="display: none;"></div>
+				<table class="form-table">
+					<tbody>
+						<tr>
+							<th scope="row"><strong><?php _e( 'Name', 'elm' ) ?></strong></th>
+							<td><input type="text" class="popup-input" name="name" placeholder="<?php _e( 'Name', 'elm' ) ?>" id="name"></td>
+						</tr>
+						<tr>
+							<th scope="row"><strong><?php _e( 'Email', 'elm' ) ?></strong></th>
+							<td><input type="email" class="popup-input" name="email" placeholder="<?php _e( 'Email', 'elm' ) ?>" id="email"></td>
+						</tr>
+					</tbody>
+				</table>
+				<p class="submit">
+					<button id="subscribe" class="button button-primary"><?php _e( 'Subscribe', 'elm' ) ?></button>
+				</p>
+			</div>
+		</div>
+		<?php
+	}
+	?>
 	<div id="tab-container">
 		<form method="post" action="options.php">
 			<?php
